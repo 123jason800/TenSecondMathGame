cf8e3a79347e9b755eba948e7d0d7f607f8a749b
jasonouyang8000 <jianjun.ouyang@lacity.org>
2020-12-17 16:33:04 -0800

    added timer

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-// var randomNumber = function()
 var gameForm = $('#game-form');
 var gameButton = $('.game-button');
 var gameInput = $('#game-input input');
-var getUserInput = function() {
-
+var gameTimerText = $('.game-timer-text');
+var gameLimitInput = $('#limit-input');
+var limitValues = $('.limit-values');
+var countDown = function(seconds) {
+    var timer = setInterval(function(){
+        seconds--;
+        gameTimerText.text(seconds);
+        if (seconds === 0) {
+            clearInterval(timer);
+        }
+    },1000);   
 }
-
 
 
 
@@ -17,15 +24,20 @@ gameButton.on('click',function(){
         current.prop("checked", true);
         gameForm.find('.active').toggleClass('active');
         $(this).toggleClass('active');     
-    
+
     }
 });
 
-
+// Ensures input is seen by user
 $(window).click(function() {
     gameInput.trigger('focus');
 })
 
+gameLimitInput.on('input',function() {
+    var percent = Math.floor((parseInt($(this).val())/50) * 100);
+    limitValues.text($(this).val());
+    $(this).css("background",`linear-gradient(90deg, rgba(230,145,233,1) 0%, rgba(29,29,155,1) ${percent}%)`);
+});
 
 var generateRandomInt = function() {
 
@@ -35,8 +47,9 @@ var generateRandomInt = function() {
 
 
 
+
 // On Load
 
-gameInput.trigger('focus');
+
 
 
